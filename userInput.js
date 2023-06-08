6627339155887ed357e6b50ce952b2b70e3caa7d
mamacbride <michaelmcbride85@gmail.com>
2023-06-08 11:19:20 -0400

    further submit button reactivity

--- userInput.js
+++ userInput.js
@@ -4,6 +4,18 @@ const textArea = document.querySelector('.textarea-field');
 // Get reference to button
 const button = document.querySelector('#submit-button');
 
+// Hide button until user types in the text area
+textArea.addEventListener('input', (e) => {
+  // Check if the textarea is not empty
+  if (e.target.value.trim() !== "") {
+    // Make the button visible
+    button.style.display = 'block';
+  } else {
+    // Hide the button if textarea is empty
+    button.style.display = 'none';
+  }
+});
+
 // Store data in local storage when user types
 button.addEventListener('click', (e) => {
     const userInput = textArea.value;
