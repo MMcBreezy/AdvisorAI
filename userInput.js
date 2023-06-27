9408a7c0f8560da284afd8ef381f6f61fb11a20c
mamacbride <michaelmcbride85@gmail.com>
2023-06-27 10:43:56 -0400

    clean up comments

--- userInput.js
+++ userInput.js
@@ -1,27 +1,19 @@
-// Get reference to text area
 const textArea = document.querySelector(".textarea-field");
 
-// Get reference to button
 const button = document.querySelector("#submit-button");
 
-// Hide button until user types in the text area
 textArea.addEventListener("input", (e) => {
-  // Check if the textarea is not empty
   if (e.target.value.trim() !== "") {
-    // Make the button visible
     button.style.display = "block";
   } else {
-    // Hide the button if textarea is empty
     button.style.display = "none";
   }
 });
 
-// Store data in local storage when user clicks submit
 button.addEventListener("click", (e) => {
   const userInput = textArea.value;
   localStorage.setItem("userInput", userInput);
   console.log(userInput);
 });
 
-// Setting focus on the textarea when page loads
 document.getElementById("user-input").focus();
